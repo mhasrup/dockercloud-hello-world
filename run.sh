f67c2425a981f117fabebde61c2f83fa6683418b
mhasrup <mhasrup@gmail.com>
2019-06-23 15:13:07 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,9 @@ CONF_FILE=/etc/nginx/nginx.conf
 sed -i "s/#LISTEN_PORT#/${LISTEN_PORT}/" ${CONF_FILE}
 
 php-fpm -d variables_order="EGPCS" && exec nginx -g "daemon off;"
+
+for num in {1..50}
+do
+ echo "Number: $num"
+ sleep 1
+done
