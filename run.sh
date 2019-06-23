7f84510d3d88b2b1e37de2f0159e12115cab5da7
mhasrup <mhasrup@gmail.com>
2019-06-24 00:40:24 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,3 +7,10 @@ sed -i "s/#LISTEN_PORT#/${LISTEN_PORT}/" ${CONF_FILE}
 php-fpm -d variables_order="EGPCS" && exec nginx -g "daemon off;"
 
 echo "Hi Laxman in TC??????"
+
+#!/bin/bash
+for i in {1..100}
+do
+echo "Welcome $i times"
+sleep 1
+done
