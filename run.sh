71c32a856d515d99e3d695d6759f776e3e72281a
mhasrup <mhasrup@gmail.com>
2019-06-23 15:15:44 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,4 @@ sed -i "s/#LISTEN_PORT#/${LISTEN_PORT}/" ${CONF_FILE}
 
 php-fpm -d variables_order="EGPCS" && exec nginx -g "daemon off;"
 
-for num in {1..50}
-do
- echo "Number: $num"
- sleep 1
-done
+echo "Hi Laxman in TC??????"
